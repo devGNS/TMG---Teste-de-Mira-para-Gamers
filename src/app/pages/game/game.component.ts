804ef4367c4f72c1fc2fb8c8242fd7a3b32daee5
Gustavo Naves <gnavesouza@gmail.com>
2022-04-15 12:41:01 -0300

    feat criacao de um botao que aparece na tela randomicamente, ao ser clicado conta os acertos

--- src/app/pages/game/game.component.ts
+++ src/app/pages/game/game.component.ts
@@ -4,37 +4,127 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-game',
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.scss'],
-})
-export class GameComponent implements OnInit {
+})export class GameComponent implements OnInit {
+
   marginTop: number = 0;
   marginLeft: number = 0;
-  tempo : number = 1000;
-  contador: number = 10;
+  margins:string='';
+  tempo : number = 800;
+  contador: number = 0;
+  tempoRestante: number = 30;
+  marginLeftVet: number[] = [];
+  marginTopVet: number[] = [];
+  posicaoAtual: number = 0;
+  qtdeAcertos:number = 0;
 
-  ngStyle : string =`{ 'margin-top': ${this.marginTop} + '%', 'margin-left':  ${this.marginLeft} + '%'}`
   constructor() {}
 
   ngOnInit(): void {
+    this.preencheVetor();
+    this.temporizadorTempoTotal();
+    this.temporizador(this.posicaoAtual, this.tempo);
+  }
+
+  preencheVetor(){
+    console.log('teste');
+
+    for(let i = 0 ; i<200 ; i++){
+      this.marginLeftVet.push( Math.random() * 100);
+      this.marginTopVet.push(Math.random() * 500);
+      i++;
+      console.log(i);
+    }
+  }
+
+  clickBotao(){
+    this.qtdeAcertos++;
     this.mudaPosicao();
   }
 
-  async mudaPosicao() {
-      await this.temporizador(1000).then(() => {
-        this.marginLeft = Math.random() * 100;
-        this.marginTop = Math.random() * 100;
-        this.contador = this.contador - 1
-        console.log(this.contador);
-        if(this.contador >0){
-          this.mudaPosicao();
-        }
-      });
-
+   mudaPosicao() {
+      this.posicaoAtual++;
+      this.marginLeft = this.marginLeftVet[this.posicaoAtual];
+      this.marginTop = this.marginTopVet[this.posicaoAtual];
+      this.temporizador(this.posicaoAtual, this.tempo);
   }
 
-  async temporizador(tempo: number) {
-   await setTimeout(() => {
-    console.log("teste");
-
+   temporizador(posicaoRecebida: number, tempo: number) {
+    setTimeout(() => {
+    if((this.posicaoAtual == posicaoRecebida) && this.tempoRestante>0){
+      this.mudaPosicao();
+    }
     }, tempo)
   }
+
+  temporizadorTempoTotal(){
+    setTimeout(()=> {
+      if(this.tempoRestante > 0){
+        this.tempoRestante--;
+        this.temporizadorTempoTotal();
+      }
+    }, 1000)
+  }
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// export class GameComponent implements OnInit {
+//   marginTop: number = 0;
+//   marginLeft: number = 0;
+//   tempo : number = 1000;
+//   contador: number = 10;
+
+//   ngStyle : string =`{ 'margin-top': ${this.marginTop} + '%', 'margin-left':  ${this.marginLeft} + '%'}`
+//   constructor() {}
+
+//   ngOnInit(): void {
+//     this.mudaPosicao();
+//   }
+
+//    mudaPosicao() {
+//        this.temporizador(1000)
+//         this.marginLeft = Math.random() * 100;
+//         this.marginTop = Math.random() * 100;
+//         this.contador = this.contador - 1
+//         console.log(this.contador);
+
+
+
+//   }
+
+//    temporizador(tempo: number) {
+//     setTimeout(() => {
+//     if(this.contador >0){
+//       this.mudaPosicao();
+//     }
+
+
+//     }, tempo)
+//   }
+// }
